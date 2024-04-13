3e16bceea1ea16202eecdcbdf6372b36c3ffdd6a
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-04-13 15:03:58 +0530

    add conform to nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1360,6 +1360,9 @@ require("lazy").setup({
 				python = { "isort", "black" },
 				-- Use a sub-list to run only the first available formatter
 				javascript = { { "prettierd", "prettier" } },
+				typescript = { { "prettierd", "prettier" } },
+				typescriptreact = { { "prettierd", "prettier" } },
+				javacsriptreact = { { "prettierd", "prettier" } },
 			},
 			format_on_save = {
 				-- These options will be passed to conform.format()
