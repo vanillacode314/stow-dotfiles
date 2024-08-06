51c9df1423a9637c1a007f7530ee2aac04df64bb
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-08-07 05:12:49 +0530

    fix nvim plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -552,7 +552,7 @@ require("lazy").setup({
 						use_delta = true,
 						use_custom_command = nil, -- setting this implies `use_delta = false`. Accepted format is: { "bash", "-c", "echo '$DIFF' | delta" }
 						side_by_side = false,
-						diff_context_lines = vim.o.scrolloff,
+						vim_diff_opts = { ctxlen = 999 },
 						entry_format = "state #$ID, $STAT, $TIME",
 						mappings = {
 							i = {
