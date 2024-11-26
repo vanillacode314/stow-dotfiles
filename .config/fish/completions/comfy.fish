1dc53dda9e58e179aeef180ee9bf301dd4e60dfb
Raqueebuddin Aziz <raqueebuddin.aziz.314@gmail.com>
2024-11-26 20:57:29 +0530

    update

--- .config/fish/completions/comfy.fish
+++
@@ -1,1 +0,0 @@
-complete --command comfy --no-files --arguments "(env _COMFY_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=get-args _TYPER_COMPLETE_ARGS=(commandline -cp) comfy)" --condition "env _COMFY_COMPLETE=complete_fish _TYPER_COMPLETE_FISH_ACTION=is-args _TYPER_COMPLETE_ARGS=(commandline -cp) comfy"
